63ed5e4cf322b19c7a52de0e8ebc77b9497a8ed6
Tanner Cude <tanner4137@gmail.com>
2021-10-14 18:41:52 +0000

    corrected postinstall script

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook /home/tcude/ansible/playbooks/apt.yml --user tcude --ask-pass --ask-become-pass -i /home/tcude/ansible/hosts
+ansible-playbook /home/tcude/ansible/playbooks/postinstall.yml --user tcude --ask-pass --ask-become-pass -i /home/tcude/ansible/hosts
