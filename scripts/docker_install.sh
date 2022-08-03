45c934f065d38be39ff2ae7639ddca4ffded845b
Tanner Cude <tanner4137@gmail.com>
2022-08-03 02:21:06 +0000

    testing

--- scripts/docker_install.sh
+++ scripts/docker_install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook ~/projects/ansible/playbooks/docker_install.yml --user tcude --ask-pass --ask-become-pass -i ~/projects/ansible/hosts
+ansible-playbook ~/projects/ansible/playbooks/install_docker.yml --user tcude --ask-pass --ask-become-pass -i ~/projects/ansible/hosts
